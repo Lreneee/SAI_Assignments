363b42ef3196d224e5710be807171fd5235bb9e3
Irene de Koning <irenedekoning1@gmail.com>
2022-09-28 15:17:45 +0200

    28/09/2022 Update moves

--- Assignment1/src/Main.java
+++ Assignment1/src/Main.java
@@ -4,8 +4,11 @@ public class Main {
 
         State state = new State();
         state.read("/Users/irenedekoning/SYMBOLIC_AI/SAI_Assignments/Assignment1/data/board.txt");
+
         String boardString = state.toString();
         System.out.print(boardString);
+
+        System.out.println(state.legalMoves(1));
         //Game g=new Game();
         //g.test();
     }
--- Assignment1/src/State.java
+++ Assignment1/src/State.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Vector;
 
@@ -7,7 +8,7 @@ public class State {
     char[][] board;
     int[] agentX, agentY;
     int[] score;
-    int turn;
+    int turn = 0;
     int food;
 
     Vector<String> moves;
@@ -37,6 +38,17 @@ public class State {
             if (boardLine < boardHeight) {
                 for (int col = 0; col < boardWith; col++) {
                     this.board[boardLine][col] = charCharacters[col];
+
+                    //Initialize agent positions
+                    if(charCharacters[col] == 'A'){
+                        this.agentY = new int[2];
+                        this.agentX = new int[2];
+                        this.agentX[0] = boardLine;
+                        this.agentY[0] = col;
+                    } else if(charCharacters[col] == 'B'){
+                        this.agentX[1] = boardLine;
+                        this.agentY[1] = col;
+                    }
                     //System.out.print(this.board[boardLine][col]);
                 }
             }
@@ -52,49 +64,82 @@ public class State {
     }
 
     public String toString() {
-        for(int row=0; row<boardHeight; row++) {
-            for(int col = 0; col < boardWith; col++){
+        for (int row = 0; row < boardHeight; row++) {
+            for (int col = 0; col < boardWith; col++) {
                 System.out.print(this.board[row][col]);
             }
             System.out.println();
         }
+        System.out.print(this.agentX[0]);
+        System.out.println(this.agentY[0]);
+        System.out.print(this.agentX[1]);
+        System.out.println(this.agentY[1]);
+
         return null;
     }
 
     public State copy() {
-        return null;
+        State state = new State();
+        state.board = this.board;
+        state.agentX = this.agentX;
+
+        return state;
     }
 
     public Vector<String> legalMoves(int agent) {
-        if(board[row][column-1]!="#"){
-
-        }else if(board[row][column+1]!="#"){
-
-        } else if(board[row-1][column]!="#"){
-
-        } else if(board[row+1][column] != "#"){
-
-        } else if(board[row][column]=="*"){
-
-        } else if (board[row][column]==" "){
-            
+        Vector<String> possibleActions = new Vector<String>();
+        char row = (char)this.agentX[agent];
+        char column = (char)this.agentY[agent];
+//        char row = (char)1;
+//        char column = (char)3;
+        System.out.println(this.agentX[agent]);
+        System.out.println(this.agentY[agent]);
+        if (board[row][column-1] != '#') {
+            possibleActions.add("LEFT");
+        }if (board[row][column+1] != '#') {
+            possibleActions.add("RIGHT");
+        }if (board[row - 1][column] != '#') {
+            possibleActions.add("UP");
+        }if (board[row + 1][column] != '#') {
+            possibleActions.add("DOWN");
+        }if (board[row][column] == '*') {
+            possibleActions.add("EAT");
+        }if (board[row][column] == ' ' ||board[row][column] == 'A' || board[row][column] =='B') {
+            possibleActions.add("BLOCK");
         }
-        return null;
+        return possibleActions;
     }
 
     public Vector<String> legalMoves() {
-        return null;
+        return legalMoves(turn);
     }
 
     public void execute(String action) {
-
+        switch (action){
+            case "LEFT":
+                this.agentY[turn] = this.agentY[turn]-1;
+                break;
+            case "RIGHT":
+                this.agentY[turn] = this.agentY[turn]+1;
+                break;
+            case "UP":
+                this.agentX[turn] = this.agentX[turn]-1;
+                break;
+            case "DOWN":
+                this.agentX[turn] = this.agentX[turn]+1;
+                break;
+            case "EAT":
+                this.board[(char)this.agentX[turn]][(char)this.agentY[turn]] = ' ';
+                break;
+            case "BLOCK":
+                this.board[(char)this.agentX[turn]][(char)this.agentY[turn]] = '#';
+                break;
+            default:
+                break;
+        }
     }
 
     public boolean isLeaf() {
         return true;
-    }
-
-    public double value(int agent) {
-        return 34;
     }
 }
